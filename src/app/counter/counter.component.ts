7f92b093f5058c84b121e272cbbcf12a6d5d5b94
sujith <sujithkpg@gmail.com>
2024-08-12 21:07:36 +0530

    css changes

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -14,18 +14,5 @@ import { selectCounter } from './selector';
   styleUrl: './counter.component.css'
 })
 export class CounterComponent {
-  private countSubject = new BehaviorSubject<number>(0);
-  count$ = this.countSubject.asObservable();
-
-  increment() {
-    this.countSubject.next(this.countSubject.value + 1);
-  }
-
-  decrement() {
-    this.countSubject.next(this.countSubject.value - 1);
-  }
-
-  reset() {
-    this.countSubject.next(0);
-  }
+ 
 }
